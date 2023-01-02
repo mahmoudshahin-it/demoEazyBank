d0aae362ab0b49afb9b72a8db06d1fb9e9fad610
mahmoudshahin-it <mahmoudshahin.it@gmail.com>
2023-01-02 06:29:58 +0200

    Initial commit - empty project

---
+++ src/main/java/com/eazybank/demoeazybank/DemoEazyBankApplication.java
@@ -0,0 +1,13 @@
+package com.eazybank.demoeazybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEazyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEazyBankApplication.class, args);
+	}
+
+}
